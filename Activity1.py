3b81a975e563368c59faf2438960f6a75d049807
Vivienne126 <vivienne.singh@cns.ac.in>
2026-09-19 12:56:33 +0530

    New12

--- Activity1.py
+++ Activity1.py
@@ -20,4 +20,24 @@ sns.histplot(data=df,x="body_mass_g" , bins=20 , color="pink")
 plt.title("penguin")
 plt.xlabel("Body mass(grams)")
 plt.ylabel("Count")
+#plt.show()
+
+
+#Kdeplot and fitting distribution
+
+# sns.kdeplot(data=df,x="flipper_length_mm" , hue="species" , fill=True)
+# plt.title("Flipper length shape by speices (KDE)")
+# plt.xlabel("Flipper length(mm)")
+#plt.show()
+# sns.histplot(data=df,x="flipper_length_mm", kde=True , color="coral")
+# plt.title("Flipper length-Histogram with KDE curve")
+# plt.xlabel("Flipper length(mm)")
+# plt.ylabel("count")
+# plt.show()
+
+sns.scatterplot(data=df,x="flipper_length_mm", y="body_mass_g")
+plt.title("Flipper length vs body mass by seices")
+plt.xlabel("Flipper length(mm)")
+plt.ylabel("body mass(grams)")
 plt.show()
+
